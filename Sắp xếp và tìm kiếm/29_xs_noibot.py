36a64dc92c079bd69e1bc52d9828af1905bef013
TanDat_Eg <72129432+tandat-1305@users.noreply.github.com>
2021-05-07 02:50:41 +0700

    Add files via upload

---
+++ Sắp xếp và tìm kiếm/29_xs_noibot.py
@@ -0,0 +1,12 @@
+def xs_noi_bot(nums):
+    bien_thaydoi = True
+    while bien_thaydoi:
+        bien_thaydoi = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                bien_thaydoi = True
+
+list_random = [5, 2, 1, 8, 4]
+xs_noi_bot(list_random)
+print(list_random)
